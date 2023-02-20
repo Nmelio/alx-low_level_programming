3a141d276b5feff7e6b907a7776b9da7175ccd46
Nmelio <nmelichukwuokeyonochie@gmail.com>
2023-02-20 12:37:54 +0100

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers of base 10 start from 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
