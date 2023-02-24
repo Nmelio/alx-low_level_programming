5e6d8849e6bb085df0c60e068574cbc29d93699b
Nmelio <nmelichukwuokeyonochie@gmail.com>
2023-02-24 01:30:48 -0800

    nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,19 +4,20 @@
  * more_numbers - prints 10 times the numbers, from 0 to 14
  * followed by a new line
  */
+
 void more_numbers(void)
-
 {
-	int f, g;
+	int i, j;
 
-	for (f = 0; f < 10; f++)
+	for (i = 0; i < 10; i++)
 	{
-		for (g = 0; g < 15; g++)
+		for (j = 0; j < 15; j++)
 		{
-			if (g >= 10)
+			if (j >= 10)
 			{
-				_putchar(g / 10 + '0');
-				_putchar(g % 10 + '0');
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+
 			}
 		}
 		_putchar('\n');
