933ed24a31c9ec707526b0114e02318224c832d7
Nmelio <nmelichukwuokeyonochie@gmail.com>
2023-02-27 22:25:09 +0100

    Arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,22 +16,4 @@ void print_rev(char *s)
 		_putchar(s[index]);
 	}
 	_putchar('\n');
-}#include "holberton.h"
-
- /**
-  * print_rev - This function prints a reverse string
-  *
-  * @s: This is the input string
-  */
-void print_rev(char *s)
-{
-	int index;
-
-	for (index = 0; s[index] != '\0'; index++)
-		;
-	for (index = index - 1; s[index] != '\0'; index--)
-	{
-		_putchar(s[index]);
-	}
-	_putchar('\n');
 }
