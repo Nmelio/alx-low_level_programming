4511d148b3f2763b04821a6d53e8adb71be98a81
Nmelio <nmelichukwuokeyonochie@gmail.com>
2023-03-08 12:47:43 +0100

    string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ * @x: pointer to imputed array
+ *
+ * Return: x
+ */
+
+char *string_toupper(char *x)
+{
+	for (int i = 0; i < '\0'; i++)
+	{
+		if (x[i] >= 'a' && x[i] <= 'z')
+		{
+			x[i] -= 32;
+		}
+	}
+}
