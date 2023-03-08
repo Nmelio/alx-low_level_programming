94bfcb9df843f3a21335d25c3a0bce078d1a146a
Nmelio <nmelichukwuokeyonochie@gmail.com>
2023-03-08 11:59:52 +0100

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_strncpy - function that copies a string
+ *_strncpy - copies a string
  *
  * @dest: input value
  * @src: input value
@@ -10,7 +10,7 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int j;
 
 	for (j = 0; (j < n && src[j] != '\0'); j++)
 	{
