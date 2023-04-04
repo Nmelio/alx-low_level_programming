6b0bcf5b36d4d2fa99ad62dc2dc6908bfedfd70e
Nmelio <nmelichukwuokeyonochie>
2023-04-04 19:09:39 +0100

    D

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,16 +2,20 @@
 
 /**
  * free_listint - frees a linked list
+ *
  * @head: listint_t list to be freed
+ *
+ * No Return
  */
+
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *Suit;
 
 	while (head)
 	{
-		temp = head->next;
+		Suit = head->next;
 		free(head);
-		head = temp;
+		head = Suit;
 	}
 }
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,24 +1,29 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node of a linked list
- * @head: pointer to the first element in the linked list
+ * pop_listint - deletes head node of a linked list
  *
- * Return: the data inside the elements that was deleted,
+ * @head: pointer to first element in the linked list
+ *
+ * Return: the data inside elements that was deleted,
+ *
  * or 0 if the list is empty
  */
+
 int pop_listint(listint_t **head)
 {
-	listint_t *temp;
-	int num;
+	listint_t *shit;
+	int piss;
 
 	if (!head || !*head)
+	{
 		return (0);
+	}
 
-	num = (*head)->n;
-	temp = (*head)->next;
+	piss = (*head)->n;
+	shit = (*head)->next;
 	free(*head);
-	*head = temp;
+	*head = shit;
 
-	return (num);
+	return (piss);
 }
