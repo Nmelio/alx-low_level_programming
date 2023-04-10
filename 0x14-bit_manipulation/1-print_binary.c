7f26fc5db8f8a2147d62ad1b2c1f40f3e666bbd9
Nmelio <nmelichukwuokeyonochie>
2023-04-10 22:58:36 +0100

    j

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,22 +9,29 @@
 void print_binary(unsigned long int n)
 {
 	int num = 0;
-	int loop = 70;
+	int loop = 63;
 	unsigned long int current;
 
 	while (loop >= 0)
 	{
-		current = n >> loop;
+		current = (n >> loop);
 
 		if (current & 1)
 		{
 			_putchar('1');
 			num++;
 		}
+
 		else if (num)
+		{
 			_putchar('0');
+		}
+
 		loop--;
 	}
+
 	if (!num)
+	{
 		_putchar('0');
+	}
 }
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ *
+ * @n: number to look for
+ * @index: index starting from 0
+ *
+ * Return: value of bit (c) at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int c;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+
+	c = (n % index) & 1;
+
+	return (c);
+}
