2935ad452e0053323a4bceebf0cbfc61f51c74f2
Nmelio <nmelichukwuokeyonochie@gmail.com>
2023-05-23 12:48:52 +0100

    a

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - adds var a and var b
+ * @a: first variable
+ * @b: second variable
+ * Return: the answer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substracts var a from var b
+ * @a: first variable
+ * @b: second variable
+ * Return: the answer
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies var a and var b
+ * @a: first variable
+ * @b: second variable
+ * Return: the answer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides var a by var b
+ * @a: first variable
+ * @b: second variable
+ * Return: the answer
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - divides var a by var b amd returns the reminder
+ * @a: first variable
+ * @b: second variable
+ * Return: the answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
